b4200f65e781e88ea9c2cba843628017569284d2
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2024-02-06 09:50:12 -0300

    Pokedex 06/02/24

---
+++ Pokedex/PokemonAPI/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PokemonAPI
+{
+    partial class Pokedex
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConsultaPokemon = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtNomePokemon = new System.Windows.Forms.TextBox();
+            this.lbID = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbTipo = new System.Windows.Forms.Label();
+            this.lbPeso = new System.Windows.Forms.Label();
+            this.lbAltura = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConsultaPokemon
+            // 
+            this.btnConsultaPokemon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultaPokemon.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnConsultaPokemon.Location = new System.Drawing.Point(196, 391);
+            this.btnConsultaPokemon.Name = "btnConsultaPokemon";
+            this.btnConsultaPokemon.Size = new System.Drawing.Size(81, 40);
+            this.btnConsultaPokemon.TabIndex = 0;
+            this.btnConsultaPokemon.Text = "Consultar";
+            this.btnConsultaPokemon.UseVisualStyleBackColor = true;
+            this.btnConsultaPokemon.Click += new System.EventHandler(this.btnConsultaPokemon_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(272, 257);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtNomePokemon
+            // 
+            this.txtNomePokemon.Location = new System.Drawing.Point(12, 402);
+            this.txtNomePokemon.Multiline = true;
+            this.txtNomePokemon.Name = "txtNomePokemon";
+            this.txtNomePokemon.Size = new System.Drawing.Size(161, 27);
+            this.txtNomePokemon.TabIndex = 2;
+            // 
+            // lbID
+            // 
+            this.lbID.AutoSize = true;
+            this.lbID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbID.Location = new System.Drawing.Point(12, 301);
+            this.lbID.Name = "lbID";
+            this.lbID.Size = new System.Drawing.Size(22, 16);
+            this.lbID.TabIndex = 3;
+            this.lbID.Text = "ID";
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbName.Location = new System.Drawing.Point(11, 321);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(48, 16);
+            this.lbName.TabIndex = 4;
+            this.lbName.Text = "Nome";
+            // 
+            // lbTipo
+            // 
+            this.lbTipo.AutoSize = true;
+            this.lbTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTipo.Location = new System.Drawing.Point(12, 337);
+            this.lbTipo.Name = "lbTipo";
+            this.lbTipo.Size = new System.Drawing.Size(39, 16);
+            this.lbTipo.TabIndex = 5;
+            this.lbTipo.Text = "Tipo";
+            // 
+            // lbPeso
+            // 
+            this.lbPeso.AutoSize = true;
+            this.lbPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPeso.Location = new System.Drawing.Point(12, 353);
+            this.lbPeso.Name = "lbPeso";
+            this.lbPeso.Size = new System.Drawing.Size(43, 16);
+            this.lbPeso.TabIndex = 6;
+            this.lbPeso.Text = "Peso";
+            // 
+            // lbAltura
+            // 
+            this.lbAltura.AutoSize = true;
+            this.lbAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAltura.Location = new System.Drawing.Point(12, 369);
+            this.lbAltura.Name = "lbAltura";
+            this.lbAltura.Size = new System.Drawing.Size(47, 16);
+            this.lbAltura.TabIndex = 7;
+            this.lbAltura.Text = "Altura";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(296, 273);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Pokedex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(304, 452);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbAltura);
+            this.Controls.Add(this.lbPeso);
+            this.Controls.Add(this.lbTipo);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.lbID);
+            this.Controls.Add(this.txtNomePokemon);
+            this.Controls.Add(this.btnConsultaPokemon);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Pokedex";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pokedex do perigo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConsultaPokemon;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtNomePokemon;
+        private System.Windows.Forms.Label lbID;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbTipo;
+        private System.Windows.Forms.Label lbPeso;
+        private System.Windows.Forms.Label lbAltura;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Pokedex/PokemonAPI/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PokemonAPI
+{
+    public partial class Pokedex : Form
+    {
+        //const = cria uma variável cujo o valor é fixo, ou seja, uma constante somente leitura
+        //em outras palavras ele busca e verifica o nome do pokemon escrito (no site).
+        
+        private const string ApiBaseUrl = "https://pokeapi.co/api/v2/pokemon/";
+        //Define-se uma constante chamada ApiBaseUrl que armazena o URL base da API do PokeAPI.
+        public Pokedex()
+        {
+            InitializeComponent();
+        }
+        //Ele é marcado como async porque faz chamadas assíncronas para a API.
+        //Isso significa que o método pode executar operações que podem ser realizadas de forma assíncrona
+        //que levar algum tempo para serem concluídas sem bloquear o thread principal.
+        //ele executa o programa sem esperar,
+        private async void btnConsultaPokemon_Click(object sender, EventArgs e)
+        {
+            //Você pode digitar o nome do pokemon desejado,e o comando TRIM.. 
+            //remove todos os caracteres de espaço em branco antes ou depois do nome digitado.
+            string pokemonName = txtNomePokemon.Text.Trim();
+            //Obtém o texto digitado no campo, e remove quaisquer espaços em branco extras ao redor.
+            //Condição
+            
+            if (!string.IsNullOrEmpty(pokemonName))
+            { //Faz uma Verificação se o nome do Pokémon digitado não está vazio ou nulo.
+                //Try tenta executar..
+            
+                try //Ele permite que você escreva código que pode gerar exceções
+                    // neste caso é usado para lidar com exceções que podem ocorrer ao consultar a API do PokeAPI
+                {
+                    //string apiUrl = $"{ApiBaseUrl} faz a verificação do nome com o site
+                    // ToLower converte as letras em minusculas
+                    //Constrói a URL completa para consultar informações do Pokémon na API, convertendo o nome do Pokémon para minúsculas.
+                    string apiUrl = $"{ApiBaseUrl}{pokemonName.ToLower()}/";
+                    // $ Permite que você insira expressões C# diretamente em uma string formatada // $ Permite você junta string com variaveis
+                    //sem a necessidade de concatenar strings manualmente.
+
+                    PokemonData pokemonData = await GetPokemonData(apiUrl);
+                    
+                    //await é usado para esperar a conclusão de uma operação assíncrona sem bloquear o thread atual.
+                    //Espera para obter os dados do Pokémon da API de forma assíncrona.
+                    
+                    if (pokemonData != null)
+                    {
+                        DisplayPokemonInfo(pokemonData); //para exibir as informações do Pokémon no formulário.
+                    }
+                    else
+                    {
+                        //Caixa de mensagem exibe a mensagem se o Pokémon não for encontrado
+                        MessageBox.Show("Pokémon não encontrado.");
+                    }
+                }
+                catch (Exception ex) // captura essas exceções e executa um código alternativo 
+                                     //Representa erros que ocorrem durante a execução do aplicativo 
+                {
+                    MessageBox.Show($"Erro ao consultar a API: {ex.Message}"); //Exibe uma caixa de mensagem se ocorrer um erro ao consultar a API.
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite o nome de um Pokémon.");
+            }
+        }
+
+        private void DisplayPokemonInfo(PokemonData pokemonData)
+        {
+            // Exibir informações do Pokémon
+            
+            lbID.Text = $"ID: {pokemonData.id}";
+            lbName.Text = $"Nome: {pokemonData.name}";
+            lbTipo.Text = $"Tipo: {string.Join(", ", pokemonData.types.Select(t => t.type.name))}";
+            //Esta parte do código (", ") está unindo os elementos dessa lista de nomes de tipos em uma única string, separando-os por vírgula e espaço
+            //Resumindo, essa linha de código configura o texto do Label lbTipo para exibir os nomes dos tipos de Pokémon separados por vírgula e espaço, após o texto "Tipo:".
+            //Se um Pokémon tiver mais de um tipo, eles serão exibidos separados por vírgula e espaço.
+            lbPeso.Text = $"Peso: {pokemonData.weight} kg";
+            lbAltura.Text = $"Altura: {pokemonData.height} m";
+
+            // Exibir imagem do Pokémon
+            DisplayPokemonImage(pokemonData.sprites.front_default);
+        }
+        // Async É usada para fazer uma solicitação HTTP
+        // para a API do PokeAPI para obter informações sobre um Pokémon.Isso permite que a aplicação
+        // Windows Forms continue respondendo a eventos do usuário enquanto aguarda a resposta da solicitação HTTP,
+        // evitando que ela pareça congelada ou não responsiva.
+        
+        private async Task<PokemonData> GetPokemonData(string apiUrl) 
+        
+        { //Este método faz uma solicitação assíncrona à API do PokeAPI para obter os dados do Pokémon.
+        //método chamado GetPokemonData que recebe uma string apiUrl como parâmetro e retorna uma tarefa (Task) 
+        //que produzirá um objeto do tipo PokemonData quando concluída 
+        //Indica o tipo específico de objeto que será retornado pela tarefa.
+        
+            using (HttpClient client = new HttpClient())
+            { //Uma biblioteca que garante que o objeto HttpClient seja liberado corretamente após o uso.
+
+                //passando seu parâmetro.. REQUISIÇÃO 
+                HttpResponseMessage response = await client.GetAsync(apiUrl); 
+                //envia uma solicitação HTTP GET assíncrona para a URL especificada em apiUrl usando um objeto HttpClient, 
+                //aguarda a conclusão da solicitação e, em seguida, armazena a resposta HTTP resultante no objeto response.
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync(); //Lê o conteúdo da resposta da API como uma string JSON.//JSON ESTRUTURA OU LISTA DE DADOS / INFORMAÇÕES
+                    //instale atravez do NuGet Newtonsoft.Json
+                    return Newtonsoft.Json.JsonConvert.DeserializeObject<PokemonData>(jsonResponse);
+                    //Converte a string JSON em um objeto PokemonData usando a biblioteca 
+                }
+            }
+
+            return null;
+        }
+
+        private void DisplayPokemonImage(string imageUrl) //Exibe a imagem do Pokémon no formulário.
+        { //Todos private ou public são uma função // void ele é vazio, depois retorna as informações dentro dos parenteses.
+
+
+            if (!string.IsNullOrEmpty(imageUrl)) //IsNUllOrEmpty  se não for nulo ou vazio
+            {//Define e determina a largura e a altura da área do controle onde a imagem será exibida.
+                pictureBox1.Size = new Size(275, 260);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; //Significa que a imagem será esticada ou reduzida para se ajustar à área do controle (Stretch reduzir)
+                pictureBox1.Load(imageUrl); //exiba a imagem obtida da URL. // formato da imagem
+            }
+        }
+
+        //Classes
+        public class PokemonData //Define uma classe para representar os dados de um Pokémon, incluindo seu nome, ID, tipos, peso, altura e sprites.
+        {
+            public string name { get; set; } //Get permite obter o valor da propriedade //um método getter retorna seu valor, enquanto um método setter o define ou atualiza.
+            public int id { get; set; } //Set permite definir o valor da propriedade // "set" significa "colocar, estabelecer" (um valor ao campo); "get" significa "obter" (o valor do campo).
+            public List<TypeData> types { get; set; } //é uma estrutura de dados que armazena uma coleção de elementos de um tipo específico. 
+            public float weight { get; set; } //Peso
+            public float height { get; set; } //Altura
+            public Sprites sprites { get; set; }  //nome,id,peso,altura são objetos
+        }
+
+        public class TypeData
+        { //Define a classe para representar os tipos de um Pokémon.
+            public Type type { get; set; }
+        }
+
+        public class Type
+        { //Ima classe para representar um tipo específico de um Pokémon.
+            public string name { get; set; }
+        }
+
+        //Classe / biblioteca
+        public class Sprites
+        { //Define uma classe para representar os sprites (imagens) de um Pokémon.
+            public string front_default { get; set; }
+        }
+    }
+
+}
